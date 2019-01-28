e9b6d5af498a4ff1ef2c9455314e12223fe27689
Rendon Elsesser <rendonelsesser@gmail.com>
2019-01-28 12:29:01 -0500

    created bookmark model

---
+++ models/Bookmark.js
@@ -0,0 +1,14 @@
+const mongoose = require('../db/connection.js')
+const Schema = mongoose.Schema
+
+const Bookmark = new Schema ({
+    text: String,
+    url: String,
+    text: String,
+    createdAt: {
+        type:Date,
+        default: Date.now()
+    }
+})
+
+module.exports = mongoose.model({Bookmark})  //DOES THIS NEED TO BE {}
