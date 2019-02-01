ee6b162ac96b8a50269db347601231efd3866e5a
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-01 09:59:17 -0500

    added db connection for mlab

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
-
-mongoose.connect("mongodb://localhost/rembr", { useNewUrlParser: true })
-
 mongoose.Promise = Promise
+
+
+if (process.env.NODE_ENV == "production") {
+    mongoose.connect(process.env.MLAB_URL)
+  } else {
+    mongoose.connect("mongodb://localhost/rembr", { useNewUrlParser: true })
+  }
+  
+
 
 module.exports = mongoose
 
